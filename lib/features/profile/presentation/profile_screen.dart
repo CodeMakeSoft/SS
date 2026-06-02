361cce535d3fb6673ac3d3309b9d9e44a0a19aa5
E33-a <emax03736@gmail.com>
2026-06-02 03:19:57 -0600

    fix: correccion en vista de profile

--- lib/features/profile/presentation/profile_screen.dart
+++ lib/features/profile/presentation/profile_screen.dart
@@ -312,7 +312,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ],
         ),
         body: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 120),
           child: Column(
             children: [
               // USER CARD (Tech Style)
